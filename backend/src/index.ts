9d72d00518310191d8f69b9fb1f40ebcc529d3fe
Rafael <rafa.arraez.gue@gmail.com>
2025-02-09 12:21:40 -0400

    chore: mongoose connect

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,22 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db';
 
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.route('/').get((req, res) => {
+    res.send('Server is running');
 });
 
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
